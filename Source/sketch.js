c8a8311666b4de80d810c03dc6b244cf6bec01b4
Jspsun <Jspsun@gmail.com>
2017-03-07 17:03:31 -0500

    modified api key access

--- Source/sketch.js
+++ Source/sketch.js
@@ -16,7 +16,7 @@ function preload() {
 
 
   //MAPKEY is a constant stored in a config file which is not included in the repo. Get your own key :)
-  mapimg = loadImage("https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/" + clat + "," + clon + "," + zoom + ",0,0/" + mapWidth + "x" + mapWidth + "?access_token=" + key);
+  mapimg = loadImage("https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/" + clat + "," + clon + "," + zoom + ",0,0/" + mapWidth + "x" + mapWidth + "?access_token=pk.eyJ1IjoianNwc3VuIiwiYSI6ImNpeno1YncycDAwbW0yd2tmYmdteHUydHYifQ.EgbNT1BzOhbP0TFTC2paMQ");
 
   earthquakes = loadStrings('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.csv');
 }
